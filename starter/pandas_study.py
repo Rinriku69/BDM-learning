b960041e29546bace073cf76fccb5e9a794a6411
Rinriku69 <siripoohkim@gmail.com>
2025-11-28 21:15:22 +0700

    pandas learn

--- starter/pandas_study.py
+++ starter/pandas_study.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-student_scores = pd.read_csv('student_score.csv')
+'''student_scores = pd.read_csv('student_score.csv')
 pd.DataFrame(student_scores)
 student_scores = student_scores.set_index('Student')
 
@@ -22,4 +22,22 @@ plt.title('Score Comparison')
 plt.xlabel('Student Name')
 plt.ylabel('Score')
 
-plt.show()
+plt.show()'''
+
+
+arrays = np.array([[1,2,3],[4,5,6],[7,8,9]])
+col = ['colA','colB','colC']
+
+df = pd.DataFrame(arrays,columns=col)
+df['Student'] = ['Studenta','Studentb','Studentc']
+df = df.set_index('Student')
+print(df)
+print('-'*20)
+
+print(df.iloc[0,2])
+print('-'*20)
+print(df.values)
+print(df.index[0])
+print(df.columns[0])
+print(df.columns.tolist())
+print(df.index.tolist())
