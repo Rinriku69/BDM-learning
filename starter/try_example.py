22a5f4f0db2de959a93f0198d77e5e14d18a79f9
Rinriku69 <siripoohkim@gmail.com>
2025-11-29 22:17:19 +0700

    try more example

--- starter/try_example.py
+++ starter/try_example.py
@@ -8,6 +8,13 @@ print( [item for item in [1, 2, 3] if item == 1])
 
 print([item*2 for item in "Hello"])
 print([i*item for i,item in enumerate([1,2,3])])
+print({item:item*2 for item in [1, 2, 3]})
+print({i:j for i in "Hello" for j in [1,2,3]}) #nested loop 
+'''for i in "Hello":
+    for j in [1, 2, 3]:
+        dict[i] = j'''
+
+
 
 a = {
     'name': "Jacob",
@@ -16,3 +23,5 @@ a = {
 
 for key,value in a.items():
     print(f"{key} :{value}")
+    
+    
